ee4e17ce08a592aa0d1d8ed93ec2862e6994171a
djhernandez-p <79471361+djhernandez-p@users.noreply.github.com>
2021-11-11 03:30:18 -0600

    Prueba funcional
    Se tiene una prueba funcional con algunos detalles estéticos y de funcionalidad

--- src/app/components/Sidebar/Sidebar.component.ts
+++ src/app/components/Sidebar/Sidebar.component.ts
@@ -7,13 +7,12 @@ import { JsonService } from "src/app/json.service";
     styleUrls: ['./Sidebar.component.css']
 })
 export class Sidebar {
-    menu: Array<any> = [];
+    menu: Array<any> = []; //Se define l variable menu que sera la que contenga los elementos a desplegar del menú
     constructor(public json: JsonService) {
         this.json.getJson('https://proyectatufuturo.app:7444/menu').subscribe(res => {
-            res.menu.forEach(element => {
-                this.menu.push(element);
+            res.menu.forEach(element => {//Se itera sobre el resultado devuelto del response
+                this.menu.push(element);//Se agrega cada elemento a menu
             })
-            console.log(this.menu);
         })
     }
 }
